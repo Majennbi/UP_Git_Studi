0780bf641dfb708e79bd7ab38dd60f0f8db557d4
Majennbi <85588366+Majennbi@users.noreply.github.com>
2022-12-24 10:37:34 +0100

    add new pizza

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'mediterranea': {
+        'ingredients': {'parmesan': 2, 'pepper': 4, 'onion': 3},
+        'price': 12,
     }
 }
